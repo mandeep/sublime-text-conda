3583a5e3866606f8b5aee84c1f6c6d82e8486890
mandeep <mandeep@users.noreply.github.com>
2017-05-07 11:14:21 -0500

    Conda plugin for ST3

---
+++ commands.py
@@ -0,0 +1,15 @@
+import sublime
+import sublime_plugin
+
+
+class CondaCommand(sublime_plugin.WindowCommand):
+    """Contains all of the methods that will be inherited by other commands."""
+
+    def __init__(self):
+        """Initialize values from the settings file."""
+        self.settings = sublime.load_settings('conda.sublime-settings')
+        self.executable = self.settings.get('executable')
+        self.environments = self.settings.get('environment_directory')
+
+    def find_conda_environments(self):
+        """Find all conda environments in the specified directory."""
