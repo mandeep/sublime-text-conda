88991f78f6a7c076af4f48e6853deb1684b9185b
mandeep <mandeep@users.noreply.github.com>
2017-05-29 00:05:50 -0500

    Added create and remove commands. Changed new command to create. Changed executable from conda to python path

--- commands.py
+++ commands.py
@@ -13,7 +13,8 @@ class CondaCommand(sublime_plugin.WindowCommand):
     def find_conda_environments(self):
         """Find all conda environments in the specified directory."""
         directory = os.path.expanduser(SETTINGS.get('environment_directory'))
-        return [environment for environment in os.listdir(directory)]
+        return [[environment, os.path.join(directory, environment)]
+                for environment in os.listdir(directory)]
 
 
 class ActivateCondaEnvironmentCommand(CondaCommand):
@@ -29,3 +30,50 @@ class ActivateCondaEnvironmentCommand(CondaCommand):
             self.window.show_quick_panel(self.find_conda_environments(), "")
         except AttributeError:
             sublime.status_message("No conda environments found.")
+
+
+class CreateCondaEnvironmentCommand(CondaCommand):
+    """Contains the methods needed to create a conda environment."""
+
+    def run(self):
+        """Display 'Conda: Create' in Sublime Text's command palette.
+
+        When 'Conda: Create' is clicked by the user, Sublime's text input
+        box will show allowing the user to input the name of environment.
+        This environment name is then passed to the create_environment
+        method.
+        """
+        self.window.show_input_panel('Conda Environment Name:', '',
+                                     self.create_environment, None, None)
+
+    def create_environment(self, environment):
+        """Create a conda environment in the envs directory."""
+        python = os.path.expanduser(SETTINGS.get('executable'))
+        cmd = [python, '-m', 'conda', 'create', '--name', environment, '-y']
+        self.window.run_command('exec', {'cmd': cmd})
+
+
+class RemoveCondaEnvironmentCommand(CondaCommand):
+    """Contains the methods needed to remove a conda environment."""
+
+    def run(self):
+        """Display 'Conda: Remove' in Sublime Text's command palette.
+
+        When 'Conda: Removed' is clicked by the user, the command
+        palette whill show all conda environments available for removal.
+        The index of the selected environment is then passed to the
+        remove_environment method"
+        """
+        try:
+            self.window.show_quick_panel(self.find_conda_environments(),
+                                         self.remove_environment)
+        except AttributeError:
+            sublime.status_message("No conda environments found.")
+
+    def remove_environment(self, index):
+        """Remove a conda environment from the envs directory."""
+        if index != -1:
+            python = os.path.expanduser(SETTINGS.get('executable'))
+            environment = self.find_conda_environments()[index][0]
+            cmd = [python, '-m', 'conda', 'remove', '--name', environment, '--all', '-y']
+            self.window.run_command('exec', {'cmd': cmd})
